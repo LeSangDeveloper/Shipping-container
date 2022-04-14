dcc74a1002e4adae347a8f0a1e30130575ef7f00
Lê Sang <42503707+LeSangDeveloper@users.noreply.github.com>
2022-04-14 23:03:00 +0700

    intergrate shipping container class with iso 6436

--- shipping.py
+++ shipping.py
@@ -1,5 +1,7 @@
-class ShippingContainer:
+import iso6346
 
+
+class ShippingContainer:
     next_serial = 1337
 
     @classmethod
@@ -7,6 +9,13 @@ class ShippingContainer:
         result = cls.next_serial
         cls.next_serial += 1
         return result
+
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(
+            owner_code=owner_code,
+            serial=str(serial).zfill(6)
+        )
 
     @classmethod
     def _create_with_empty(cls, owner_code, contents=[]):
@@ -19,4 +28,18 @@ class ShippingContainer:
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code
         self.contents = contents
-        self.serial = self._generater_serial()
+        self.bic = self._make_bic_code(
+            owner_code=owner_code,
+            serial=ShippingContainer._generate_serial()
+        )
+
+
+class RefrigeratorShippingContainer(ShippingContainer):
+
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(
+            owner_code=owner_code,
+            serial=str(serial).zfill(6),
+            category="R"
+        )
